14651579a40095a4a94d62428270925635bdf18d
Sascha Lisson <sl@slisson.de>
2022-04-24 11:24:15 +0200

    ...

--- src/commonMain/kotlin/org/modelix/incremental/DependencyGraph.kt
+++ src/commonMain/kotlin/org/modelix/incremental/DependencyGraph.kt
@@ -3,44 +3,24 @@ package org.modelix.incremental
 class DependencyGraph<KeyT> {
     private val nodes: MutableMap<KeyT, Node> = HashMap()
 
-    private fun getOrAddComputedNode(key: KeyT) = nodes.getOrPut(key) { ComputedNode(key) }
-    private fun getOrAddExternalNode(key: KeyT) = nodes.getOrPut(key) { ExternalNode(key) }
+    private fun getOrAddNode(key: KeyT) = nodes.getOrPut(key) { Node(key) }
 
     fun addDependency(from: KeyT, to: KeyT) {
-        (getOrAddNode(from) as ComputedNode).addDependency(getOrAddNode(to))
+        getOrAddNode(from).addDependency(getOrAddNode(to))
     }
 
     fun removeDependency(from: KeyT, to: KeyT) {
         val fromNode = nodes[from] ?: return
         val toNode = nodes[to] ?: return
-        (fromNode as ComputedNode).removeDependency(toNode)
+        fromNode.removeDependency(toNode)
         if (!fromNode.isConnected()) nodes.remove(from)
         if (!toNode.isConnected()) nodes.remove(to)
     }
 
     fun contains(key: KeyT) = nodes.containsKey(key)
 
-    private abstract inner class Node(val key: KeyT) {
-        protected val reverseDependencies: MutableSet<Node> = HashSet()
-
-        fun addReverseDependency(dependency: Node) {
-            reverseDependencies += dependency
-        }
-
-        fun removeReverseDependency(dependency: Node) {
-            reverseDependencies -= dependency
-        }
-
-        fun getReverseDependencies(): Set<Node> = reverseDependencies
-
-        fun isRoot() = reverseDependencies.isEmpty()
-
-        open fun isConnected() = reverseDependencies.isNotEmpty()
-
-        open fun getTransitiveDependencies(result: MutableSet<Node> = HashSet()): Set<Node> = emptySet()
-    }
-
-    private inner class ComputedNode(key: KeyT) : Node(key) {
+    private inner class Node(val key: KeyT) {
+        private val reverseDependencies: MutableSet<Node> = HashSet()
         private val dependencies: MutableSet<Node> = HashSet()
 
         fun addDependency(dependency: Node) {
@@ -56,20 +36,26 @@ class DependencyGraph<KeyT> {
 
         fun getDependencies(): Set<Node> = dependencies
 
-        override fun isConnected() = dependencies.isNotEmpty() || super.isConnected()
-
-        override fun getTransitiveDependencies(result: MutableSet<Node>): Set<Node> {
+        fun getTransitiveDependencies(result: MutableSet<Node>): Set<Node> {
             if (!result.contains(this)) {
                 result += dependencies
                 dependencies.forEach { it.getTransitiveDependencies(result) }
             }
             return result
         }
+        fun addReverseDependency(dependency: Node) {
+            reverseDependencies += dependency
+        }
 
-    }
+        fun removeReverseDependency(dependency: Node) {
+            reverseDependencies -= dependency
+        }
 
-    private inner class ExternalNode(key: KeyT) : Node(key) {
+        fun getReverseDependencies(): Set<Node> = reverseDependencies
 
+        fun isRoot() = reverseDependencies.isEmpty()
+
+        fun isConnected() = dependencies.isNotEmpty() || reverseDependencies.isNotEmpty()
     }
 }
 
