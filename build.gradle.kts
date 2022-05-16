0d63eca49129647ae78fb20590c7e82d8c1621aa
Sascha Lisson <sl@slisson.de>
2022-05-16 10:08:13 +0200

    Github action for publishing tags

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,9 @@ plugins {
     kotlin("multiplatform") version "1.6.20"
     id("org.jetbrains.kotlinx.benchmark") version "0.4.2"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
+    `maven-publish`
+    id("com.palantir.git-version") version "0.13.0"
 }
-
-group = "org.modelix"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -97,6 +96,39 @@ benchmark {
             include("RecursiveSum")
             exclude("RecursiveSumLarge")
             exclude("RecursiveSumNonIncremental")
+        }
+    }
+}
+
+group = "org.modelix"
+description = "Incremental computation engine"
+
+val versionFile = projectDir.resolve("version.txt")
+version = if (versionFile.exists()) {
+    versionFile.readText().trim()
+} else {
+    val gitVersion: groovy.lang.Closure<String> by extra
+    gitVersion()
+}
+if (!project.findProperty("ciBuild")?.toString().toBoolean()) {
+    version = "$version-SNAPSHOT"
+}
+println("Version: $version")
+
+publishing {
+    repositories {
+        if (project.hasProperty("artifacts.itemis.cloud.user")) {
+            maven {
+                name = "itemisNexus3"
+                url = if (version.toString().contains("SNAPSHOT"))
+                          uri("https://artifacts.itemis.cloud/repository/maven-mps-snapshots/")
+                      else
+                          uri("https://artifacts.itemis.cloud/repository/maven-mps-releases/")
+                credentials {
+                    username = project.findProperty("artifacts.itemis.cloud.user").toString()
+                    password = project.findProperty("artifacts.itemis.cloud.pw").toString()
+                }
+            }
         }
     }
 }
