b05fdc1efe44ff17cfb367c612d73e6a212d0fab
Sascha Lisson <sascha.lisson@itemis.de>
2024-03-11 15:03:28 +0100

    build: ensure compatibility to JDK 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,9 @@ kotlin {
     *  To find out how to configure the targets, please follow the link:
     *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
 
-    jvm()
+    jvm {
+        jvmToolchain(11)
+    }
     js(IR) {
         browser {}
         nodejs {
