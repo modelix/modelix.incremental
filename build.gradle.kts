cdd2179cf38fbac2ae3058cc963d0bad6e0d1dae
Sascha Lisson <sascha.lisson@itemis.de>
2024-05-17 10:25:57 +0200

    feat: new method TrackableMap.removeWhere

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,16 @@ plugins {
     `maven-publish`
 }
 
+val versionFile = projectDir.resolve("version.txt")
+version = if (versionFile.exists()) {
+    versionFile.readText().trim()
+} else {
+    val ci = !project.findProperty("ciBuild")?.toString().toBoolean()
+    val gitVersion: groovy.lang.Closure<String> by extra
+    gitVersion() + (if (ci) "-SNAPSHOT" else "")
+}
+println("Version: $version")
+
 allprojects {
     apply(plugin = "maven-publish")
     repositories {
@@ -11,6 +21,7 @@ allprojects {
     }
 
     group = "org.modelix"
+    version = rootProject.version
 
     publishing {
         repositories {
@@ -45,13 +56,3 @@ allprojects {
     }
 
 }
-
-val versionFile = projectDir.resolve("version.txt")
-version = if (versionFile.exists()) {
-    versionFile.readText().trim()
-} else {
-    val ci = !project.findProperty("ciBuild")?.toString().toBoolean()
-    val gitVersion: groovy.lang.Closure<String> by extra
-    gitVersion() + (if (ci) "-SNAPSHOT" else "")
-}
-println("Version: $version")
